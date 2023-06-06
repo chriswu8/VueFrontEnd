3f716f15f2c99b5a09d51a60ff43de311ce9aea4
Chris Wu <chriswu@Chriss-MacBook-Pro.local>
2023-06-06 10:32:15 -0700

    initial commit

---
+++ modal-project/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click me</button>
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: "App",
+  // components: {
+  //   HelloWorld,
+  // },
+  data() {
+    return {
+      title: 'My First Vue App =D'
+    }
+  },
+  methods: {
+    handleClick()
+    {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active') // adds a class to ref="name" tag
+      this.$refs.name.focus() // the focus goes to ref="name" tag
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ modal-project/src/components/Modal.vue
@@ -0,0 +1,27 @@
+// Modal component to be used in App component
+
+<template>
+  <div class="backdrop">
+    <div class="modal">
+      <p>Modal Content</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 200px auto;
+  background: white;
+  border-radius: 10px;
+}
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);   /* 0.5 makes it semi-transparent */
+  width: 100%;
+  height: 100%;
+}
+</style>
+
